3780b07af2c79e8a6102067323afaa2c683473f2
Nyasha Madondo <nyashm24@gmail.com>
2022-06-06 05:36:09 -0700

    start

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <unistd.h>
+
 
 
 /**
@@ -12,9 +14,7 @@ int main(void)
 
 {
 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 return (1);
 
