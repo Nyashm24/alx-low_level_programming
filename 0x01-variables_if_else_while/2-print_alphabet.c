2591c037e9c9bfeded7b958f7384d292deb9a980
Nyasha Madondo <nyashm24@gmail.com>
2022-06-06 06:16:13 -0700

    sattat

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,14 @@
 
 
 /**
-* main - Prints the alphabet in lowercase.
-*
-* Return: Always 0.
-*/
+
+ * main - Prints the alphabet in lowercase.
+
+ *
+
+ * Return: Always 0.
+
+ */
 
 int main(void)
 
