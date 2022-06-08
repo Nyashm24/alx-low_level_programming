20d5b8b9ac208842911076636609912a1bba6372
Nyasha Madondo <nyashm24@gmail.com>
2022-06-08 09:36:47 -0700

    sas

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,16 +1,24 @@
 #include "main.h"
 /**
-* main - check the code for Holberton School students.
-*
-* Return: Always 0.
+* print_sign - a function that prints the sign of a number
+* @n: number input
+* Return: 1 prints + if n > 0, 0 prints 0 if n = 0, -1 prints - if n < 0
 */
-int main(void)
+int print_sign(int n)
 {
-int r;
-r = print_sign(98);
-if (r < 0)
-r *= -1;
-_putchar(r + 0);
-_putchar('\n');
+if (n > 0)
+{
+_putchar ('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar ('0');
 return (0);
+}
+else
+{
+_putchar ('-');
+return (-1);
+}
 }
