f4e1ce08ace729d7509ccc3679fadcbe10482abe
Nyasha Madondo <nyashm24@gmail.com>
2022-06-08 09:31:10 -0700

    sas

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,35 +1,16 @@
 #include "main.h"
-
-
-
 /**
-
- * main - check the code for Holberton School students.
-
- *
-
- * Return: Always 0.
-
- */
-
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
-
 {
-
 int r;
-
-
-
 r = print_sign(98);
-
 if (r < 0)
-
 r *= -1;
-
 _putchar(r + 0);
-
-_putchar(n);
-
+_putchar('\n');
 return (0);
-
 }
