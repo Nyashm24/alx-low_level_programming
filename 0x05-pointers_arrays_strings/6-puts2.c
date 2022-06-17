da3cbf15effa9ee1d452f37909a4b2d99043b657
Nyasha Madondo <nyashm24@gmail.com>
2022-06-17 08:15:37 -0700

    sas

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,49 +1,22 @@
 #include "main.h"
-
-
-
 /**
-
- * puts2 - Entry point
-
- * Description - A function that prints every other character
-
- * of a string, starting with the first character,
-
- * followed by a new line
-
- * *@str: the function accepts an input saved into str
-
- * Return: Always 0 (Success)
-
- */
-
+* puts2 - Entry point
+* Description - A function that prints every other character
+* of a string, starting with the first character,
+* followed by a new line
+* *@str: the function accepts an input saved into str
+* Return: Always 0 (Success)
+*/
 void puts2(char *str)
-
 {
-
 int i = 0;
-
-
-
-while (str[i] != 0)
-
+while (str[i] != '0')
 {
-
-if (i % 2 == 0)
-
+if (i % 2 == '0')
 {
-
 _putchar(str[i]);
-
 }
-
 i++;
-
-
-
 }
-
-_putchar(n);
-
+_putchar('\n');
 }
