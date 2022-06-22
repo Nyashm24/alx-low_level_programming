442099da90c382e3f2dc9daa6ba4e9327cfef463
Nyasha Madondo <nyashm24@gmail.com>
2022-06-22 05:58:54 -0700

    sas

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,35 +1,17 @@
- #include "holberton.h"
-
-#include <math.h>
-
+#include "main.h"
 /**
-
- * _pow_recursion - func that returns the value of x raised to the power of y
-
- * @x: paramater for the number
-
- * @y: number to be raised to
-
- * Return: Always 0.
-
- */
-
+*_pow_recursion - function that returns the power of number
+*@x: number
+*@y: power
+*Return: the number raised to the power
+*/
 int _pow_recursion(int x, int y)
-
 {
-
-if (y < 0)
-
-{
-
-return (-1);
-
-}
-
-else if (y == 0)
-
+if (y > -1 && y <= 1 && y != 0)
+return (x);
+if (y == 0)
 return (1);
-
+if (y <= -1)
+return (-1);
 return (x * _pow_recursion(x, y - 1));
-
 }
