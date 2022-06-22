5652aea38196729820a076b80b35ae8cfd3f79ab
Nyasha Madondo <nyashm24@gmail.com>
2022-06-22 05:37:16 -0700

    start

---
+++ 0x08-recursion/0-main.c
@@ -0,0 +1,23 @@
+ #include "holberton.h"
+
+
+
+/**
+
+ * main - check the code for Holberton School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+
+{
+
+README.md _puts_recursion("Betty Holberton");
+
+README.md return (0);
+
+}
