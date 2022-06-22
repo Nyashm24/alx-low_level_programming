aad9983b68e185e15246e41a4a7e1ff15fdf224d
Nyasha Madondo <nyashm24@gmail.com>
2022-06-22 05:56:52 -0700

    asa

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,41 +1,15 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
-
- * factorial - function that returns the factorial of a given number
-
- * @n: number to do factorialization on
-
- * Return: Always 0.
-
- */
-
+*factorial - calculate factorial of a given number
+*@n: number
+*
+*Return: the factorial of a number
+*/
 int factorial(int n)
-
 {
-
-if (n < 0)
-
-{
-
-return (-1);
-
-}
-
-else if (n <= 1)
-
-{
-
+if (n >= 0 && n <= 1)
 return (1);
-
-}
-
-else
-
-{
-
+if (n < 0)
+return (-1);
 return (n * factorial(n - 1));
-
-}
-
 }
