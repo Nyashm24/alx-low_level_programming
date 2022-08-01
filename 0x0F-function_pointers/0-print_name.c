d9dc328107988ec2c43d9a9c4993da3493c55559
Nyasha Madondo <nyashm24@gmail.com>
2022-08-01 07:40:07 -0700

    functions

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
-#include "function_pointers.h"
 /**
-* print_name - prints a name
-* @name: string
-* @f: function
+* print_name - Prints a name
+* @name: The name to print
+* @f: A function that makes changes on a string.
 */
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
-README.md f(name);
+f(name);
 }
